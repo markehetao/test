9ee3d2070f80ea6ac3c8f0836b278eedcbfcd400
ht <ht@htdeMacBook-Pro.local>
2016-02-19 23:01:02 +0800

    commit changes gitTest

---
+++ gitTest/gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by ht on 16/2/19.
+//  Copyright (c) 2016年 ht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitTest/gitTest/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  gitTest
+//
+//  Created by ht on 16/2/19.
+//  Copyright (c) 2016年 ht. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"111");
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
